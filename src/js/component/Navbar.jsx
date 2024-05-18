c09ec1dea123e02e7cd017c3726e42ec59d2030b
Omar Hidalgo <omar_ehr@outlook.com>
2024-05-18 02:45:54 +0000

    ADD dynamic cards

--- src/js/component/Navbar.jsx
+++ src/js/component/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   <div className="container-fluid">
     <a className="navbar-brand text-light" href="#">Navbar</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
+    <i class="fas fa-bars text-white"></i>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,26 +5,39 @@ import Card from "./Card";
 
 //create your first component
 const Home = () => {
-	return (
-		<>
-			<Navbar />
-			<div className="container">
-				<Jumbotron />
-				<div className="container mx-auto row gap-2 justify-content-center">
-					<Card/>
-					<Card/>
-					<Card/>				
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>
-					<Card/>				
-					<Card/>
-				</div>
-			</div>
-		</>
-	);
+  const listaDeEstudiantes = [
+    { nombre: "Angie" },
+    { nombre: "Sebastian" },
+    { nombre: "Omar" },
+    { nombre: "Gustavo" },
+    { nombre: "Jorge" },
+    { nombre: "Valentina" },
+    { nombre: "Luis" },
+    { nombre: "Cesar" },
+    { nombre: "Alexis" },
+    { nombre: "Jose" },
+    { nombre: "Omar" },
+    { nombre: "Jorge" },
+  ];
+
+  return (
+    <>
+      <Navbar />
+      <div className="container">
+        <Jumbotron />
+        <div className="container mx-auto row gap-2 justify-content-center">
+          {listaDeEstudiantes.map((estudiante, index) => {
+            return (
+              <Card
+                nombre={estudiante.nombre}
+                key={`estudiante-${estudiante}-${index}`}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 console.log("Something changed!");
 export default Home;
-
